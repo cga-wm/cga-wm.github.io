e8f19930e1e7aa20880bd5a7b306c66c8b881d7c
dt-woods <davisler@gmail.com>
2020-07-08 23:37:34 -0400

    create geoethics slide show

--- course/adv-gis/lectures/geoethics/build.R
+++ course/adv-gis/lectures/geoethics/build.R
@@ -1,4 +1,4 @@
 library("rmarkdown")
 library("revealjs")
 setwd("./")
-render("ethics.Rmd", output_format="revealjs::revealjs_presentation")
+render("index.Rmd", output_format="revealjs::revealjs_presentation")
